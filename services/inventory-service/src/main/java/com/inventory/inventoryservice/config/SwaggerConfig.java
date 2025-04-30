2b6498e7ea07318e97afb2dbe3be8fa74729e41e
Agbobli Isaac <agbobliisaackwadzo@gmail.com>
2025-04-30 15:09:41 +0000

    feat: Add springdoc-openapi dependency and update Swagger configuration to OpenAPI 3

--- services/inventory-service/src/main/java/com/inventory/inventoryservice/config/SwaggerConfig.java
+++ services/inventory-service/src/main/java/com/inventory/inventoryservice/config/SwaggerConfig.java
@@ -1,60 +1,36 @@
 package com.inventory.inventoryservice.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.*;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.inventory.inventoryservice.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .securityContexts(Collections.singletonList(securityContext()))
-                .securitySchemes(Collections.singletonList(apiKey()));
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Inventory Service API")
-                .description("API documentation for the Inventory Management Service")
-                .version("1.0.0")
-                .contact(new Contact("Development Team", "https://inventory-system.example.com", "dev@inventory-system.example.com"))
-                .build();
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header");
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-                .securityReferences(defaultAuth())
-                .forPaths(PathSelectors.regex("/api/.*"))
-                .build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    public OpenAPI inventoryServiceAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Inventory Service API")
+                        .description("API documentation for the Inventory Management Service")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Development Team")
+                                .url("https://inventory-system.example.com")
+                                .email("dev@inventory-system.example.com")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")));
     }
 }
